f3ee43477f0d1786752df559e3d1fd49ad5a5451
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-10-09 16:41:18 +0200

    build
    adding game functionalities

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,116 @@
-import Image from 'next/image'
+'use client';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [xTurn, setXTurn] = useState(true);
+    const [boardData, setBoardData] = useState({
+        0: '',
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+        5: '',
+        6: '',
+        7: '',
+        8: '',
+    });
+    const [won, setWon] = useState(false);
+    const [wonCombo, setWonCombo] = useState([]);
+    const [modalTitle, setModalTitle] = useState('');
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const WINNING_COMBO = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const checkWinner = () => {
+        WINNING_COMBO.map((values) => {
+            const [a, b, c] = values;
+            if (boardData[a] && boardData[a] === boardData[b] && boardData[a] === boardData[c]) {
+                setWon(true);
+                setWonCombo([a, b, c]);
+                setModalTitle(`Player ${!xTurn ? 'X' : 'O'} Won!!!`);
+                return;
+            }
+        });
+    };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    useEffect(() => {
+        checkWinner();
+    }, [boardData]);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    const updateBoardData = (index) => {
+        if (!boardData[index] && !won) {
+            let value = xTurn === true ? 'X' : 'O';
+            setBoardData({ ...boardData, [index]: value });
+            setXTurn(!xTurn);
+        }
+    };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    const reset = () => {
+        setBoardData({
+            0: '',
+            1: '',
+            2: '',
+            3: '',
+            4: '',
+            5: '',
+            6: '',
+            7: '',
+            8: '',
+        });
+        setXTurn(true);
+        setWon(false);
+        setWonCombo([]);
+        setModalTitle('');
+    };
+
+    return (
+        <>
+            <h1 className='text-center text-5xl m-10'>Tic Tac Toe</h1>
+            <div className='flex flex-col align-middle mt-6 '>
+                <div className='text-center text-2xl font-semibold'>
+                    <p>{xTurn === true ? 'X Turn' : 'O Turn'}</p>
+                    {/* <p>{`Game Won:${won}`}</p> */}
+                </div>
+                <div className='grid grid-cols-3 gap-2 text-center pr-20 pb-20 pl-20 '>
+                    {[...Array(9)].map((v, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className={`bg-gray-500 rounded-lg p-5 text-center text-5xl leading-10 font-bold w-100 h-20 cursor-pointer shadow-md ${
+                                    wonCombo.includes(index) ? 'bg-green-300' : ''
+                                }`}
+                                onClick={() => {
+                                    updateBoardData(index);
+                                }}
+                            >
+                                {boardData[index]}
+                            </div>
+                        );
+                    })}
+                </div>
+                <div
+                    className={`shadow-lg w-3/6 text-center rounded-2xl flex flex-col align-middle p-6 fixed top-1/3 left-1/2 bg-white text-black translate-y-2/4 translate-x-2/4 scale-0 transition duration-200 ${
+                        modalTitle ? 'show' : ''
+                    }`}
+                >
+                    <div className='modal__title'>{modalTitle}</div>
+                    <button
+                        onClick={reset}
+                        className='h-16 bg-gray-500 border-none font-bold shadow-lg'
+                    >
+                        New Game
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
+1;
